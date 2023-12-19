d62e0ad2042b9114aac893965f9d3a736d2f97d0
LoKe112 <ivan1200100@mail.ru>
2023-12-20 01:35:12 +0400

    add all graphics

--- graphics.py
+++ graphics.py
@@ -3,20 +3,19 @@ import numpy as np
 
 def out(path):
     x1, y1, z1 = np.loadtxt(f"rand_data_{path}_sort.txt", unpack=True)
-    # Построение графика
+    
 
     plt.plot(x1, y1, marker='o', linestyle=':')
     plt.plot(x1, z1, marker='o', linestyle=':')
 
-    x2, y2, z2 = np.loadtxt(f"sorted_{path}.txt", unpack=True)
-    # Построение графика
+    x2, y2, z2 = np.loadtxt(f"sorted_data_{path}_sort.txt", unpack=True)
+    
 
     plt.plot(x2, y2, marker='o', linestyle=':')
     plt.plot(x2, z2, marker='o', linestyle=':')
 
-    x3, y3, z3 = np.loadtxt(f"unsorted_{path}.txt", unpack=True)
-    # Построение графика
-
+    x3, y3, z3 = np.loadtxt(f"unsorted_data_{path}_sort.txt", unpack=True)
+    
     plt.plot(x3, y3, marker='o', linestyle=':')
     plt.plot(x3, z3, marker='o', linestyle=':')
 
--- tests/tests.cc
+++ tests/tests.cc
@@ -32,15 +32,45 @@ TEST(natural_two_way_sort, test_1) {
 
 TEST(bubble_sort, aboba) {
 	int seed = 983747;
-	fill_file_random_seed(1, "E:\\AISD3\\rand_data_bubble_sort.txt", 1, seed, 1);	
+	fill_file_random_seed(1, "E:\\AISD3\\rand_data_bubble_sort.txt", 2, seed, 2);	
 }
 
 TEST(shaker_sort, aboba) {
 	int seed = 983747;
-	fill_file_random_seed(2, "E:\\AISD3\\rand_data_shaker_sort.txt", 1, seed, 1);
+	fill_file_random_seed(2, "E:\\AISD3\\rand_data_shaker_sort.txt", 2, seed, 2);
 }
 
-TEST(natural_two_way_sort, aboba) {
+/*TEST(natural_two_way_sort, aboba) {
 	int seed = 983747;
-	fill_file_random_seed(3, "E:\\AISD3\\rand_data_natural_two_way_sort.txt", 1, seed, 1);
+	fill_file_random_seed(3, "E:\\AISD3\\rand_data_natural_two_way_sort.txt", 1, seed, 2);
+}*/
+
+/*TEST(bubble_sort, aboba_1) {
+	int seed = 983747;
+	fill_file_random_seed(1, "E:\\AISD3\\sorted_data_bubble_sort.txt", 1, seed, 0);
 }
+
+TEST(shaker_sort, aboba_1) {
+	int seed = 983747;
+	fill_file_random_seed(2, "E:\\AISD3\\sorted_data_shaker_sort.txt", 1, seed, 0);
+}
+
+TEST(natural_two_way_sort, aboba_1) {
+	int seed = 983747;
+	fill_file_random_seed(3, "E:\\AISD3\\sorted_data_natural_two_way_sort.txt", 1, seed, 0);
+}
+*/
+/*TEST(bubble_sort, aboba_1) {
+	int seed = 983747;
+	fill_file_random_seed(1, "E:\\AISD3\\unsorted_data_bubble_sort.txt", 1, seed, 1);
+}
+
+TEST(shaker_sort, aboba_1) {
+	int seed = 983747;
+	fill_file_random_seed(2, "E:\\AISD3\\unsorted_data_shaker_sort.txt", 1, seed, 1);
+}
+
+TEST(natural_two_way_sort, aboba_1) {
+	int seed = 983747;
+	fill_file_random_seed(3, "E:\\AISD3\\unsorted_data_natural_two_way_sort.txt", 1, seed, 1);
+}*/
